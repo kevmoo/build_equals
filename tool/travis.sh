fc7766436e83458a6e25a5f8d5c9679417f24bbb
Kevin Moore <kevmoo@google.com>
2019-06-20 10:02:23 -0700

    more cleanup

--- tool/travis.sh
+++ tool/travis.sh
@@ -22,10 +22,6 @@ for PKG in ${PKGS}; do
     echo
     echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
     case ${TASK} in
-    command)
-      echo 'pub run build_runner test --delete-conflicting-outputs -- -p chrome'
-      pub run build_runner test --delete-conflicting-outputs -- -p chrome || EXIT_CODE=$?
-      ;;
     dartanalyzer_0)
       echo 'dartanalyzer --fatal-warnings --fatal-infos .'
       dartanalyzer --fatal-warnings --fatal-infos . || EXIT_CODE=$?
@@ -38,13 +34,9 @@ for PKG in ${PKGS}; do
       echo 'dartfmt -n --set-exit-if-changed .'
       dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
       ;;
-    test_0)
+    test)
       echo 'pub run test'
       pub run test || EXIT_CODE=$?
-      ;;
-    test_1)
-      echo 'pub run test --run-skipped -t presubmit-only test/ensure_build_test.dart'
-      pub run test --run-skipped -t presubmit-only test/ensure_build_test.dart || EXIT_CODE=$?
       ;;
     *)
       echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
