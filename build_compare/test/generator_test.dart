5bfabda1c8097214e8ca1a5ab771d7d80b381867
Kevin Moore <kevmoo@google.com>
2020-03-30 13:24:22 -0700

    more analysis options

--- build_compare/test/generator_test.dart
+++ build_compare/test/generator_test.dart
@@ -2,7 +2,7 @@ import 'package:build_compare/src/build_compare_generator.dart';
 import 'package:path/path.dart' as p;
 import 'package:source_gen_test/source_gen_test.dart';
 
-void main() async {
+Future<void> main() async {
   initializeBuildLogTracking();
   final reader = await initializeLibraryReaderForDirectory(
     p.join('test', 'test_inputs'),
